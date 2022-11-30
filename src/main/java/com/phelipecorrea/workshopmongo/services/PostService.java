703913e25ee61ec2c318840814a2632d3dd3ccb7
Phelipe Correa <pires858@gmail.com>
2022-11-30 13:32:14 -0300

    Consulta simples com query methods

--- src/main/java/com/phelipecorrea/workshopmongo/services/PostService.java
+++ src/main/java/com/phelipecorrea/workshopmongo/services/PostService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.phelipecorrea.workshopmongo.domain.Post;
-import com.phelipecorrea.workshopmongo.domain.User;
 import com.phelipecorrea.workshopmongo.repository.PostReposotory;
 import com.phelipecorrea.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -20,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
