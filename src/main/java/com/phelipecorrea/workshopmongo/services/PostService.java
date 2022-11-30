2d525f68850eee1121e59b1379102a9b6aa4017e
Phelipe Correa <pires858@gmail.com>
2022-11-30 13:04:21 -0300

    Obtendo um post por id

---
+++ src/main/java/com/phelipecorrea/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.phelipecorrea.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phelipecorrea.workshopmongo.domain.Post;
+import com.phelipecorrea.workshopmongo.domain.User;
+import com.phelipecorrea.workshopmongo.repository.PostReposotory;
+import com.phelipecorrea.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostReposotory repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
