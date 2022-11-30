414379d0620d607b913b64482f338a0219319a54
Phelipe Correa <pires858@gmail.com>
2022-11-30 13:42:47 -0300

    Consulta simples com @Query

--- src/main/java/com/phelipecorrea/workshopmongo/repository/PostReposotory.java
+++ src/main/java/com/phelipecorrea/workshopmongo/repository/PostReposotory.java
@@ -3,12 +3,16 @@ package com.phelipecorrea.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.phelipecorrea.workshopmongo.domain.Post;
 
 @Repository
 public interface PostReposotory extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
